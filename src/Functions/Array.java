ccaa78b5a438a3b9869384977cc8a9aa5752838e
CaptainShark007 <hugo.lautaro.brocal@gmail.com>
2024-05-10 22:17:44 -0300

    adding files

---
+++ src/Functions/Array.java
@@ -0,0 +1,25 @@
+package Functions;
+
+import java.util.Arrays;
+
+public class Array {
+  public static void display(int[] array){
+    for (int e:array){
+      System.out.print(e+" ");
+    }
+  }
+
+  public static int[] delete(int[] array, int element){
+    int[] newArray = new int[array.length-1];
+    int i = 0;
+
+    for (int e:array){
+     if(e!=element){
+       newArray[i] = e;
+       i++;
+     }
+    }
+
+    return newArray;
+  }
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import Functions.Array;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int[] array = {1,2,3,4,5,6,7,8,9};
+    int element;
+
+    System.out.println("El arreglo cargado es:");
+    Array.display(array);
+    System.out.println("\nIngrese un número a eliminar:");
+    element = scan.nextInt();
+    System.out.println("El nuevo arreglo es:");
+    Array.display(Array.delete(array,element));
+  }
+}
